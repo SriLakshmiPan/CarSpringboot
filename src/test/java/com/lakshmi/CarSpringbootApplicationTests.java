cadae7929430e465b518650d15cb29dccb8f379a
SriLakshmiPan <SriLakshmiPan@gmail.com>
2020-09-17 16:26:29 -0400

    carspringboot changes

---
+++ src/test/java/com/lakshmi/CarSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lakshmi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
